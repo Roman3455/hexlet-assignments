ecda35895aa1b4d33ef7ec67965d898adb72c3c8
@hexlet/cli <support@hexlet.io>
2025-01-28 22:40:47 +0300

    submit java-spring-boot-ru/dto

---
+++ java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,65 @@
+package exercise.controller;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.exception.ResourceNotFoundException;
+import exercise.dto.PostDTO;
+import exercise.dto.CommentDTO;
+import org.springframework.web.bind.annotation.*;
+
+// BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping
+    public List<PostDTO> index() {
+        var posts = postRepository.findAll();
+        return posts.stream()
+                .map(this::toDTO)
+                .toList();
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PostDTO show(@PathVariable Long id) {
+        var post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+        return toDTO(post);
+    }
+
+    private PostDTO toDTO(Post post) {
+        var comments = commentRepository.findByPostId(post.getId());
+        var commentsDTO = comments.stream()
+                .map(this::toDTO)
+                .toList();
+
+        var dto = new PostDTO();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setBody(post.getBody());
+        dto.setComments(commentsDTO);
+        return dto;
+    }
+
+    private CommentDTO toDTO(Comment comment) {
+        var dto = new CommentDTO();
+        dto.setId(comment.getId());
+        dto.setBody(comment.getBody());
+        return dto;
+    }
+}
+// END
